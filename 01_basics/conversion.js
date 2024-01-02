04908269a822d29abf71e55b334183447084934b
devendra <pd418964@gmail.com>
2024-01-02 14:15:51 +0530

    first commit

---
+++ 01_basics/conversion.js
@@ -0,0 +1,12 @@
+let score = "33";
+
+let scoreIsNumber = Number(score);
+console.log( typeof scoreIsNumber);  
+
+// "33" => 33
+// "33abc" => NaN
+// 1: true, 0: false
+// "hitesh" => true
+// "" => false
+
+
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,32 @@
+// JavaScript has total 8 data types
+
+// number:
+let age = 23;        //integer and float
+
+// bigint:
+
+let id =  09240472002497742;   // larges number
+
+
+// strings:
+
+const name = "devendra";  // sequence of characters
+
+// boolean:
+
+let isStudent = true;     //returns true or false
+
+//null:
+
+let earnings = null;     // explicit definition of intentional absence of object value
+
+//undefined:
+
+let career;      // didn't defined any vlaue to it
+
+//symbol:
+
+const sym1 = Symbol();   // gives unique symbol
+
+console.log(age, id, name,isStudent, earnings,career,sym1);
+
